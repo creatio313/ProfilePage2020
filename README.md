a06062d9d5f56c9fe1cbe808166ee78290423fa6
Sho Yashiro <shirotani@seginus.jp>
2019-12-31 16:56:29 +0900

    画像追加

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # YashiroWebPage2020
  Webpage of Yashiro(2020)
- マテリアルデザイン・国際化の練習
+ スクラッチによる軽量化バージョン
--- js/main.js
+++ js/main.js
@@ -21,38 +21,40 @@ document.addEventListener('DOMContentLoaded', function(){
 
 (function () {
     $(function () {
-        var clientLang = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
-        if(clientLang == "ja"){
-          setLang("ja");
-        }else{
-          setLang("en");
-        }
+      /** 多言語対応 */
+      var clientLang = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
+      if(clientLang == "ja"){
         setLang("ja");
-        $('#ja_button').click(function(){
-          setLang("ja")
-          $('#philosophyMainButton').show();
-        });
-        $('#en_button').click(function(){
-          setLang("en")
-          $('#philosophyMainButton').hide();
-        });
+      }else{
+        setLang("en");
+      }
+
+      /** 画面のフェードイン */
+      $("body").fadeIn(700);
+
+      /** ボタンクリック時に言語を変更する */
+      $('#ja_button').click(function(){
+        setLang("ja")
+        $('#philosophyMainButton').show();
+      });
+      $('#en_button').click(function(){
+        setLang("en")
+        $('#philosophyMainButton').hide();
+      });
     });
 
+    /** 言語変換関数 */
     function setLang(lang) {
-        console.log('lang=' + lang);
 
-        i18next.use(i18nextXHRBackend).init({
-            backend: {
-                loadPath: 'locales/{{lng}}/string.json'
-            },
-            debug: false,
-            defaultLng: 'ja',
-            fallbackLng: false,
-            lng: lang,
-        }, function (err, t) {
-            jqueryI18next.init(i18next, $);
-            $('[data-i18n]').localize();
-        });
+      i18next.use(i18nextXHRBackend).init({
+        backend:{loadPath: 'locales/{{lng}}/string.json'},
+        debug: false,
+        defaultLng: 'ja',
+        fallbackLng: false,
+        lng: lang,
+      }, function (err, t) {
+        jqueryI18next.init(i18next, $);
+        $('[data-i18n]').localize();
+      });
     }
-
 })();
