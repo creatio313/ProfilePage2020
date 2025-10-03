c5b53ad45383f6eade415784f929afbbb46a488f
Sho Yashiro <shirotani@seginus.jp>
2025-10-03 22:42:23 +0900

    簡易化・情報アップデート

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, parallel, watch } = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const minifyCSS = require('gulp-csso');
 
 function html() {
